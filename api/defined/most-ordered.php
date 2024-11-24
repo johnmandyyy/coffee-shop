e76e36bf5031062595997dab38919fe5dd020507
John Mandy <69840834+johnmandyyy@users.noreply.github.com>
2024-11-24 19:39:41 +0800

    Updated conditions for most ordered.

--- api/defined/most-ordered.php
+++ api/defined/most-ordered.php
@@ -20,7 +20,7 @@ function executeReport($query)
 
 $_query = "SELECT *, COUNT(*) as 'times_sold', (COUNT(*) * total_price_of_all) as 'generated_stream' FROM previous_order where previous_order.is_done = 1 GROUP BY json_order ORDER by times_sold desc LIMIT 2;";
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+if ($_SERVER['REQUEST_METHOD'] === 'GET' || $_SERVER['REQUEST_METHOD'] === 'POST') {
     $results = executeReport($_query);
     http_response_code(200);
     echo json_encode($results, JSON_PRETTY_PRINT);
